848771187893715c7d988d1fd76ae54aa8e522ff
Tatiana Hitomi Miyazaki <tatimiyazaki@gmail.com>
2017-01-03 16:43:12 -0200

    menu created

--- Biblioteca/app/app.ts
+++ Biblioteca/app/app.ts
@@ -5,17 +5,33 @@ import {HomePage} from './pages/home/home';
 
 
 @Component({
-  template: '<ion-nav [root]="rootPage"></ion-nav>'
+  templateUrl: 'build/app.html'
 })
 export class MyApp {
+
+  //criação do array de páginas, informando quais os seus atributos
+  pages: Array<{component: any, title: string, icon: string}>;
+
   rootPage: any = HomePage;
 
   constructor(platform: Platform) {
+
+    //array de páginas acessadas pelo menu
+    this.pages = [
+      {component: HomePage, title: 'Home', icon: 'home'}
+    ];
+
+
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
     });
+  }
+
+  //buscanco o objeto p (página)
+  openPage(page: any): void{
+    this.rootPage = page.component;
   }
 }
 
